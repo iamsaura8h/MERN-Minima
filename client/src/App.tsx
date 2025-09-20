8e5530691d68807360ffb589f3e89816622381fb
iamsaura8h <saurabhchaudhary.net@gmail.com>
2025-09-21 03:28:23 +0530

    added react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
-export default function App() {
-  
+import {Routes,Route,Navigate} from 'react-router-dom';
+import Home from './pages/home';
+import CreateUser from './pages/createUser';
 
-  return (
-    <>
-    <div>
-      <h1 className="text-4xl">hello bhai</h1>
-    </div>
-    </>
+function App () {
+  return(
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/createUser' element={<CreateUser/>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   )
 }
+
+export default App;
